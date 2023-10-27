912647b6dbfe93342ff523833ddc74de33705d5d
santyluluaga <santyluluaga@gmail.com>
2023-10-27 21:30:59 +0200

    li

--- test_calculator.py
+++ test_calculator.py
@@ -15,7 +15,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
-
-
+    
     def test_division(self):
-        assert 8 == calculator.divide(80,10)
+        assert 8 == calculator.divide(80, 10)
